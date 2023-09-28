5a443890375db59473598d454a0f5eb78418f552
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2023-09-28 21:03:28 +0200

    Update version to 0.0.2

--- pico-automator/build.gradle.kts
+++ pico-automator/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val appGroupId = "com.github.aivanovski"
 val appArtifactId = "pico-automator"
-val appVersion = "0.0.1"
+val appVersion = "0.0.2"
 
 group = appGroupId
 version = appVersion
