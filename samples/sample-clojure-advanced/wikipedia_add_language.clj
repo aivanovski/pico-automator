26a52b1fd7cc66b687c96597a6b45347fd6f388d
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2023-10-16 23:13:23 +0200

    Add example of code reuse into advanced example

--- samples/sample-clojure-advanced/wikipedia_add_language.clj
+++ samples/sample-clojure-advanced/wikipedia_add_language.clj
@@ -1,57 +1,16 @@
 (ns wikipedia-add-language
-  (:require [picoautomator.core :refer :all]))
-
-(defn open-languages-screen
-  [automator]
-
-  (-> automator
-      (launch "org.wikipedia")
-      (tap-on {:content-desc "More"})
-      (tap-on {:text "Settings"})
-      (tap-on {:text "Wikipedia languages"}))
-
-  automator)
-
-(defn remove-language-if-added
-  [automator]
-
-  (when (visible? automator {:text "Deutsch"})
-    (-> automator
-        (tap-on {:content-desc "More options"})
-        (tap-on {:text "Remove language"})
-        (tap-on {:text "Deutsch"})
-        (tap-on {:content-desc "Delete selected items"})
-        (tap-on {:text "OK"})))
-
-  automator)
-
-(defn add-language
-  [automator]
-
-  (-> automator
-      (tap-on {:text "Add language"})
-      (tap-on {:content-desc "Search for a language"})
-      (input-text "german")
-      (tap-on {:text "Deutsch"}))
-
-  automator)
-
-(defn fail-if-language-not-visible
-  [automator]
-
-  (when-not (visible? automator {:text "Deutsch"})
-    (fail automator "Language wasn't added"))
-
-  automator)
+  (:require [picoautomator.core :refer :all]
+            [languages-screen-common :as languages]))
 
 (defn flow
   [automator]
 
   (-> automator
-      (open-languages-screen)
-      (remove-language-if-added)
-      (add-language)
-      (fail-if-language-not-visible)))
+      (launch "org.wikipedia")
+      (languages/open-screen)
+      (languages/remove-language-if-need "Deutsch")
+      (languages/add-language "Deutsch")
+      (assert-visible {:text "Deutsch"})))
 
 (defn -main
   [& args]
